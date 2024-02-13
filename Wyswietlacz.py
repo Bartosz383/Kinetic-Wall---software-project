88d7ec3fa3e679dfab357c343d44aa4adb12ab62
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-02-13 22:59:35 +0100

    Poprawa przejrzystości kodu, dodanie przesuwania w każdym kierunku, dodanie podwójnej fali

--- Wyswietlacz.py
+++ Wyswietlacz.py
@@ -1,85 +1,211 @@
 import tkinter as tk
+import tkinter.messagebox as messagebox
+import tkinter.simpledialog as simpledialog
+import time
+
 
 class DisplayWindow(tk.Tk):
     def __init__(self, modul_name):
         super().__init__()
 
         self.title(modul_name)
-        self.geometry("550x400")
+        self.geometry("1100x420")  # Zmieniony rozmiar okna
 
         # Inicjalizacja zmiennych do przechowywania stanu segmentów
-        self.segments = [[0 for _ in range(4)] for _ in range(4)]
+        self.segments = [[0 for _ in range(32)] for _ in range(16)]
 
         # Utworzenie canvas do rysowania segmentów
-        self.canvas = tk.Canvas(self, width=400, height=400)
+        self.canvas = tk.Canvas(self, width=800, height=400)  # Zmieniony rozmiar canvasa
         self.canvas.grid(row=0, column=0, rowspan=5)
 
-        # Przyciski do ustawienia wszystkich segmentów na 0, 1 oraz według wzoru
-        zero_button = tk.Button(self, text="Ustaw wszystko na 0", command=self.set_all_segments_to_zero)
-        zero_button.grid(row=0, column=1, columnspan=2)
+        self.pattern = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
 
-        one_button = tk.Button(self, text="Ustaw wszystko na 1", command=self.set_all_segments_to_one)
-        one_button.grid(row=1, column=1, columnspan=2)
+        self.create_pattern()
 
-        pattern_button = tk.Button(self, text="Ustaw według wzoru", command=self.set_segments_to_pattern)
-        pattern_button.grid(row=2, column=1, columnspan=2)
+        buttons_frame = tk.Frame(self)
+        buttons_frame.grid(row=0, column=1, rowspan=5, padx=10)
 
-        pattern0_button = tk.Button(self, text="Ustaw według wzoru", command=self.set_pattern_0)
-        pattern0_button.grid(row=3, column=1, columnspan=2)
+        self.create_button(buttons_frame, "Ustaw wszystko na 0", self.set_all_segments_to_zero)
+        self.create_button(buttons_frame, "Ustaw wszystko na 1", self.set_all_segments_to_one)
+        self.create_button(buttons_frame, "Ustaw według wzoru", self.set_segments_to_pattern)
+        self.create_button(buttons_frame, "Animuj falę", self.animate_wave)
+        self.create_button(buttons_frame, "Animuj podwójną falę", self.animate_double_wave)
+        self.create_button(buttons_frame, "Wyświetl napis Freelab", self.Freelab_text)
+        self.create_button(buttons_frame, "Przesun w dół", self.shift_down)
+        self.create_button(buttons_frame, "Przesun w górę", self.shift_up)
+        self.create_button(buttons_frame, "Przesun w lewo", self.shift_left)
+        self.create_button(buttons_frame, "Przesun w prawo", self.shift_right)
+        self.create_button(buttons_frame, "Animuj", self.animate)
 
-        # Wywołanie funkcji, aby zainicjować widok
         self.show_display()
+
+    def create_button(self, parent, text, command):
+        button = tk.Button(parent, text=text, command=command)
+        button.pack(side=tk.TOP, pady=5)
 
     def show_display(self):
         # Usunięcie istniejących prostokątów na canvasie
         self.canvas.delete("all")
 
         # Wyświetlenie aktualnego stanu segmentów za pomocą wypełnionych prostokątów
-        for i in range(4):
-            for j in range(4):
-                x1, y1 = j * 100, i * 100
-                x2, y2 = x1 + 100, y1 + 100
+        for i in range(16):
+            for j in range(32):
+                x1, y1 = j * 25, i * 25  # Zmienione rozmiary kafelków
+                x2, y2 = x1 + 25, y1 + 25
 
                 color = "black" if self.segments[i][j] == 1 else "white"
 
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
 
+    def create_pattern(self):
+        # Twój własny wzór, na przykład:
+        pattern = [
+            [0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        ]
+
+        self.segments = pattern
+        self.show_display()
+
+
     def set_all_segments_to_zero(self):
         # Ustawienie wszystkich segmentów na 0
-        self.segments = [[0 for _ in range(4)] for _ in range(4)]
+        self.segments = [[0 for _ in range(32)] for _ in range(16)]
         self.show_display()
 
     def set_all_segments_to_one(self):
         # Ustawienie wszystkich segmentów na 1
-        self.segments = [[1 for _ in range(4)] for _ in range(4)]
+        self.segments = [[1 for _ in range(32)] for _ in range(16)]
         self.show_display()
 
     def set_segments_to_pattern(self):
         # Ustawienie segmentów według określonego wzoru (np. jakieś konkretne wartości)
         # Poniżej znajduje się przykładowy wzór - można dostosować go do własnych potrzeb
         pattern = [
-            [1, 0, 0, 1],
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0]
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
         ]
 
         self.segments = pattern
         self.show_display()
 
-    def set_pattern_0(self):
-        # Ustawienie segmentów według określonego wzoru (np. jakieś konkretne wartości)
-        # Poniżej znajduje się przykładowy wzór - można dostosować go do własnych potrzeb
+    def animate_wave(self, col=31):
+        if col >= 0:
+            self.set_all_segments_to_zero()
+
+            for i in range(16):
+                self.segments[i][col] = 1
+
+            self.show_display()
+            self.after(100, self.animate_wave, col - 1)
+        else:
+            self.set_all_segments_to_zero()
+
+    def animate_double_wave(self, col1=31, col2=20):
+        if col1 >= 0 and col2 >= 0:
+            self.set_all_segments_to_zero()
+
+            for i in range(16):
+                self.segments[i][col1] = 1
+                self.segments[i][col2] = 1
+
+            self.show_display()
+            self.after(100, self.animate_double_wave, col1 - 1, max(col2 - 1, 0))
+        else:
+            self.set_all_segments_to_zero()
+
+    def Freelab_text(self):
         pattern = [
-            [0, 1, 1, 0],
-            [1, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 1, 0]
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
 
         self.segments = pattern
         self.show_display()
+
+    def shift_down(self, n=1):
+        # Przesuwanie wierszy
+        self.segments = self.segments[-n:] + self.segments[:-n]
+        self.show_display()
+
+    def shift_up(self, n=1):
+        # Przesuwanie wierszy
+        self.segments = self.segments[n:] + self.segments[:n]
+        self.show_display()
+
+    def shift_left(self, n=1):
+        # Przesuwanie kolumn
+        self.segments = [row[n:] + row[:n] for row in self.segments]
+        self.show_display()
+
+    def shift_right(self, n=1):
+        # Przesuwanie kolumn
+        self.segments = [row[-n:] + row[:-n] for row in self.segments]
+        self.show_display()
+
+    def animate(self):
+       pass
+
 
 if __name__ == "__main__":
-    display_window = DisplayWindow(modul_name="Modul 1")
+    display_window = DisplayWindow(modul_name="Wszystkie moduły")
     display_window.mainloop()
