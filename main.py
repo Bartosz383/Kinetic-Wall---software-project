62b0f5043c8138a2c1edfb28bde4db85d3f4220a
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-05-04 08:29:35 +0200

    Dodanie możliwości wyboru narzędzia z poziomu głównej aplikacji

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
-import tkinter.messagebox as messagebox
-import tkinter.simpledialog as simpledialog
-import time
-import threading
+# import tkinter.messagebox as messagebox
+# import tkinter.simpledialog as simpledialog
+# import time
+# import threading
+#
+# from Wyswietlacz import DisplayWindow
+# from DetekcjaTwarzyZKamery import main_detect
+#
+# def main():
+#     # Utwórz obiekt DisplayWindow
+#     wyswietlacz = DisplayWindow("Moje Okno")
+#
+#     # Utwórz wątek dla funkcji main_detect
+#     detect_thread = threading.Thread(target=main_detect)
+#
+#     # Uruchom wątek detekcji
+#     detect_thread.start()
+#
+#     # Uruchom główną pętlę GUI dla wyswietlacza
+#     wyswietlacz.mainloop()
+#
+# if __name__ == "__main__":
+#     main()
 
-from Wyswietlacz import DisplayWindow
-from DetekcjaTwarzyZKamery import main_detect
+import tkinter as tk
+from Obraz_Na_Zwyo import Application
 
-def main():
-    # Utwórz obiekt DisplayWindow
-    wyswietlacz = DisplayWindow("Moje Okno")
+def on_button_click():
+    # Utwórz nowe okno dla aplikacji
+    new_window = tk.Toplevel(root)
+    new_window.title("Narzędzie do transmisji obrazu")
 
-    # Utwórz wątek dla funkcji main_detect
-    detect_thread = threading.Thread(target=main_detect)
+    # Zainicjuj aplikację w nowym oknie
+    app = Application(new_window)
 
-    # Uruchom wątek detekcji
-    detect_thread.start()
+# Tworzenie głównego okna
+root = tk.Tk()
+root.title("Kinetic Wall")
 
-    # Uruchom główną pętlę GUI dla wyswietlacza
-    wyswietlacz.mainloop()
+# Tworzenie etykiety
+label = tk.Label(root, text="Wybierz narzędzie")
+label.pack(pady=10)
 
-if __name__ == "__main__":
-    main()
+# Tworzenie przycisku
+button = tk.Button(root, text="Transmisja obrazu na żywo", command=on_button_click)
+button.pack(pady=5)
+
+# Uruchomienie pętli głównej
+root.mainloop()
